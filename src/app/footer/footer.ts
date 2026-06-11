92b1818410f485519305228b6452d3768b74f4ee
lkurchan <lkurchan@itba.edu.ar>
2026-06-11 14:42:18 -0300

    fix: footer

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class FooterComponent {}
-
+export class FooterComponent {
+  @Input() color = '#003566';
+}
